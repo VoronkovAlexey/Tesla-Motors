2ff21875e13eb3ecaa2831d50bf38b37eb73cecc
Alex Voronkov <lexavoronkov007@yandex.by>
2019-10-17 23:23:57 +0300

    First page on the construction

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let burgerMenu_activate = function(){
+    document.getElementById("burgerMenu").classList.toggle("burgerMenu_active");
+    document.getElementById("sideNavBar").classList.toggle("sideNavBar_active");
+}
+
+// let sideNavBar_activate = function(){
+//     document.getElementById("sideNavBar").classList.toggle("sideNavBar_active");
+// }
 
